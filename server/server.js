842cdc7aa0ad381c43a7ebbf7a0ddc19cbd250c4
Tanner Vasica <tmanvas06@hotmail.com>
2022-04-27 14:33:12 -0600

    2 commit

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ app.get(`/`, (req,res) => {
     res.sendFile(path.join(__dirname, `index.html`))
 })
 
-const port = process.eventNames.PORT || 4003
+const port = process.eventNames.PORT || 4005
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
